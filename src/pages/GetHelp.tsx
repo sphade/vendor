6d3a463a948890f94fcc1553934a8ce99e32c4c9
sphade <sphade010@gmail.com>
2022-06-02 18:09:30 +0100

    added get help page

--- src/pages/GetHelp.tsx
+++ src/pages/GetHelp.tsx
@@ -1,12 +1,12 @@
 import { ArrowBackIcon } from "../assets/images/icons";
 import TextField from "@mui/material/TextField";
-import { Button } from "../components";
+import { BackButton, Button } from "../components";
 import { TextareaAutosize } from "@mui/material";
 
 const GetHelp = () => {
   return (
     <div className="flex gap-5 mx-auto items-start border border-primary w-fit py-20 ">
-      <img src={ArrowBackIcon} alt={ArrowBackIcon} className="" />
+      <BackButton />
 
       <div className="w-[520px]  text-center shadow-lg border border-[#BDBDBD] px-[75px] bg-secondary rounded-lg ">
         <h1 className="text-2xl text-tertiary font-semibold capitalize my-10">
@@ -38,7 +38,7 @@ const GetHelp = () => {
             message
           </h1>
           <form className="mb-20">
-            <TextField fullWidth label='subject'/>
+            <TextField fullWidth label="subject" />
             <TextareaAutosize maxLength={50} />
 
             <Button full>send</Button>
